a1242ed3f191ff0d509ba21c6b8af44d712cd544
VRX9_ <degueurce.mathieu@gmail.com>
2020-05-05 19:11:18 +0200

    premier commit de ce repo

---
+++ json1.py
@@ -0,0 +1,13 @@
+import json
+
+print("Le module JSON est prêts!")
+
+x = {
+    "name": "mathieu",
+    "password": "Asvel2019",
+    "new account": False,
+    "google account": True,
+    "discord account": None,
+    "youtube account": True,
+    
+}
